6640deec2811158cf29de7309a2b70c22d2a8ba6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-23 13:39:03 +0100

    Added a tab and a new line

--- ch.0/special_char.cpp
+++ ch.0/special_char.cpp
@@ -2,6 +2,6 @@
 
 int main()
 {
-    std::cout << "This (\") is a quote, and this (\\) is a backlash" << std::endl;
+    std::cout << "This (\") is a quote, and this (\\) is a backlash.\n\tThat was a tab" << std::endl;
     return 0;
 }
